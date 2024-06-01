25ea27f270a591767054137ac16a0fa2e5246aa1
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2024-06-01 23:50:38 +0200

    add pause before starting game

--- Test/Camera2D.gd
+++ Test/Camera2D.gd
@@ -3,16 +3,26 @@ extends Camera2D
 @export var camera_speed = 50
 @export var camera_edge_speed = 200
 @onready var player = $"../Player"
+@onready var timer = $Timer
+var timer_started = false
+
+func _ready():
+	timer.start()
 
 func _process(delta):
-	global_position.y -= camera_speed * delta
-	#print(player.global_position)
-	#print(global_position)
-	var screensize = get_viewport().size
-	var width = -1*screensize[1]/2.0
-	var idk = global_position.y + width + 100
-	#print(idk)
-	
-	if player.global_position.y < idk:
-		#print("AAAA")
-		global_position.y -= camera_edge_speed * delta
+	if timer_started:
+		global_position.y -= camera_speed * delta
+		#print(player.global_position)
+		#print(global_position)
+		var screensize = get_viewport().size
+		var width = -1*screensize[1]/2.0
+		var idk = global_position.y + width + 100
+		#print(idk)
+		
+		if player.global_position.y < idk:
+			#print("AAAA")
+			global_position.y -= camera_edge_speed * delta
+
+
+func _on_timer_timeout():
+	timer_started = true
