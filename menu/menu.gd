b9c83577cf0473bb1284a7a5b82a215463a1a5f1
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2024-06-02 15:16:07 +0200

    add quit button

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
 func _on_texture_button_pressed():
 	#print("AAAA")
 	get_tree().change_scene_to_file("res://Test/test.tscn")
